237b8ca001656d7e78d0bdc7daf31e17b869cc71
imwrdo <sheinnickolas@gmail.com>
2024-12-10 20:33:24 +0100

    fixed links

--- selenium_tests/tests.py
+++ selenium_tests/tests.py
@@ -1,8 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver # type: ignore
+from selenium.webdriver.common.by import By # type: ignore
+from selenium.webdriver.common.keys import Keys # type: ignore
+from selenium.webdriver.support.ui import WebDriverWait # type: ignore
+from selenium.webdriver.support import expected_conditions as EC # type: ignore
 import random
 
 def test_prestashop():
