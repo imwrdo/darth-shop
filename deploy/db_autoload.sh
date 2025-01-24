f1c57cddb23dca96795623f0dac52905684a7cd9
imwrdo <sheinnickolas@gmail.com>
2025-01-24 07:34:19 +0100

    fix: fixed data insertion

--- deploy/db_autoload.sh
+++ deploy/db_autoload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DB_HOST="admin-mysql_db"        # Host bazy danych (nazwa kontenera)
+DB_HOST="admin-mysql_db.1.ei63yz8cr0d9i16fo4v1203gy" # Host bazy danych (nazwa kontenera)
 DB_USER="root"                  # Nazwa użytkownika bazy danych
 DB_PASS="student"               # Hasło do bazy danych
 DB_NAME="BE_196687"             # Nazwa bazy danych do utworzenia
@@ -10,10 +10,18 @@ if [ ! -f "$DUMP_FILE" ]; then
     echo "Plik dump $DUMP_FILE nie istnieje."
     exit 1
 fi
-# Tworzenie bazy danych, jeśli nie istnieje
-docker exec -i $DB_HOST mysql -u "$DB_USER" -p"$DB_PASS" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+# Sprawdzanie, czy baza danych istnieje
+result=$(docker exec -i "$DB_HOST" mysql -u "$DB_USER" -p"$DB_PASS" -e "SHOW DATABASES LIKE '$DB_NAME';")
+if [ $? -ne 0 ]; then
+    echo "Error"
+fi
+
+if [ -n "$result" ]; then 
+    echo "Podana baza danych istnieje"
+fi
+
 # Importowanie danych z pliku dump
-docker exec -i $DB_HOST mysql -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" < "$DUMP_FILE"
+docker exec -i "$DB_HOST" mysql -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" < "$DUMP_FILE"
 # Sprawdzanie statusu operacji
 if [ $? -eq 0 ]; then
     echo "Baza danych została pomyślnie zaimportowana z pliku: $DUMP_FILE"
